54ee1314bd43201df182ccfd136485ff3bfbe945
elite_lin <elite_lin@goglobal.com.tw>
2020-11-16 18:50:59 +0800

    commit for leaving work 2020/11/16

--- HelloDotnet/ie.delegates/reactives/FunctionDelegates.cs
+++ HelloDotnet/ie.delegates/reactives/FunctionDelegates.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks; // for Task usage
 using ie.exceptions;
 using ie.errorcodes;
 
@@ -26,8 +27,8 @@ namespace ie.delegates.reactives
 
     public abstract class IeBaseConverter<T, R>: IMapFunction<T, R> {
         
-        public R convertIntoData(T input) {
-        //R IMapFunction<T,R>.convertIntoData(T input) {        
+        //R IMapFunction<T,R>.convertIntoData(T input) { // such statement will incur a building error
+        public R convertIntoData(T input) {        
             try {
                 return doConversion(input);
             }
@@ -46,5 +47,29 @@ namespace ie.delegates.reactives
         protected abstract R doConversion(T input);
     }
 
-    
+    public interface IeAsyncCallable<T> {
+        Task<T> call(); // note: no async here
+    }
+
+    public abstract class IeBaseAsyncCallable<T>: IeAsyncCallable<T> {
+        
+        //R IMapFunction<T,R>.convertIntoData(T input) { // such statement will incur a building error
+        public Task<T> call() {        
+            try {
+                return doConversion(input);
+            }
+            catch (Exception cause) {
+                Console.WriteLine("{0} - {1}", "IeBaseConverter", "Error on doConversion()");                
+                if (cause is IeRuntimeException) {
+                    throw ((IeRuntimeException) cause);
+                }
+                else {
+                    throw new IeRuntimeException(
+                        "IeBaseConverter - Error on doConversion()", cause, Base.INTERNAL_CONVERSION_ERROR);
+                }
+            }
+        }
+        
+        protected abstract R doConversion(T input);
+    }
 }
--- HelloDotnet/ie.developments/DevelopmentAndTests.cs
+++ HelloDotnet/ie.developments/DevelopmentAndTests.cs
@@ -1,5 +1,8 @@
 using System;
-using System.Collections.Generic; // need for List<T>
+using System.Collections.Generic;   // need for List<T>
+using System.Diagnostics;           // need for Stopwatch
+using System.Threading.Tasks;       // need for Parallel.For() Method
+using System.Linq;                  // need for usage of Enumerable.Sum() Method, e.g., List<int>.Sum()
 using ie.delegates.reactives;
 
 namespace ie.developments
@@ -11,8 +14,8 @@ namespace ie.developments
             this.callback = callback;
         }
 
-        public void run() {
-        //void IRunnable.run() {
+        //void IRunnable.run() { // such statement will incur a building error
+        public void run() {        
             int[] intArray = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             for(int i = 0; i < intArray.Length; i++) {
                 int item = intArray[i];
@@ -36,4 +39,68 @@ namespace ie.developments
             }
         }
     }
+
+    public abstract class AbsPrimeNumberFinder: IRunnable {
+        protected List<int> GetPrimeNumbers(int minimum, int maximum) {
+            List<int> result = new List<int>();
+            for (int i = minimum; i <= maximum; i++) {
+                if (IsPrimeNumber(i)){
+                    result.Add(i);
+                }
+            }
+            return result;           
+        }
+
+        protected async Task<List<int>> GetPrimeNumbersAsync(int minimum, int maximum) {
+            var count = maximum - minimum + 1;
+            List<int> result = new List<int>();
+
+            return await Task.Factory.StartNew(() =>
+            {
+                for (int i = minimum; i <= maximum; i++) {
+                    if (IsPrimeNumber(i)) {
+                        result.Add(i);
+                    }
+                }
+                return result;
+            });
+        }
+
+        protected bool IsPrimeNumber(int number) {
+            if (number % 2 == 0) {
+                return number == 2;
+            }
+            else {
+                var topLimit = (int)Math.Sqrt(number);            
+                for (int i = 3; i <= topLimit; i += 2) {
+                    if (number % i == 0) return false;
+                }
+                return true;
+            }
+        }
+
+        public abstract void run();
+    }
+
+    public class SyncPrimeNumberFinder: AbsPrimeNumberFinder {
+        public override void run() {
+            var sw = new Stopwatch();
+            sw.Start();
+            var primes = GetPrimeNumbers(2, 10000000);
+            Console.WriteLine("Total prime numbers: {0}\nProcess time: {1}", primes.Count, sw.ElapsedMilliseconds);
+        }
+    }
+
+    public class ParallelPrimeNumberFinder: AbsPrimeNumberFinder {
+        public override void run() {
+            var sw = new Stopwatch();
+            sw.Start();
+            const int numParts = 10;
+            var primes = new List<int>[numParts];
+            Parallel.For(0, numParts, i => primes[i] = GetPrimeNumbers(i == 0 ? 2 : i * 1000000 + 1, (i + 1) * 1000000));
+            var result = primes.Sum(p => p.Count);
+            Console.WriteLine("Total prime numbers: {0}\nProcess time: {1}", result, sw.ElapsedMilliseconds);
+        }
+    }
+        
 }

829afccaf8f4cfa027f83d03f23ec46f9da69c19
elite_lin <elite_lin@goglobal.com.tw>
2020-12-07 17:37:18 +0800

    add     public class MDateDescriptionImpl: DateDescriptionDelegate {}

--- HelloDotnet/ie.models/ModelDefinitions.cs
+++ HelloDotnet/ie.models/ModelDefinitions.cs
@@ -6,5 +6,22 @@ using ie.delegates.reactives;
 
 namespace ie.models 
 {
-    
+    public class MDateDescriptionImpl: DateDescriptionDelegate {
+      public readonly DateTime dateTime;
+
+      public readonly string description;
+
+      public MDateDescriptionImpl(DateTime dateTime, string description) { 
+         this.dateTime = dateTime;
+         this.description = description;         
+      }
+
+      public DateTime theDate => this.dateTime;
+
+      public string theDescription => this.description;
+
+      public override string ToString() {
+         return "MDateDescriptionImpl {Description: " + theDescription + ", Date: " + theDate + "}";
+      }
+   }
 }

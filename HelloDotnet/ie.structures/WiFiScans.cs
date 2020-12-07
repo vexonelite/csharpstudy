12a619c09fdebd538d3d37040ca65a812fe0508d
elite_lin <elite_lin@goglobal.com.tw>
2020-12-07 11:44:06 +0800

    add namespace ie.structures.WiFiScans {     public class SAccessPointImpl : IeDelegate {} }

---
+++ HelloDotnet/ie.structures/WiFiScans.cs
@@ -0,0 +1,51 @@
+using System;
+using ie.delegates.WiFiScans;
+using ie.widgets;
+
+
+namespace ie.structures.WiFiScans
+{
+    public class SAccessPointImpl : IeDelegate {
+        private readonly string identifier;
+        private readonly int cellType;
+        private readonly string ssid;
+        private readonly string bSSID;
+        private readonly string strength;
+        private readonly string capabilities;
+        private readonly string password;
+
+        public SAccessPointImpl(
+            string identifier, int cellType, string ssid, string bSSID, string strength, string capabilities, string password) { 
+
+            this.identifier = ( (null != identifier) && (identifier.Length > 0) ) 
+                ? identifier : IeUtils.generateRandomStringViaGuid();            
+            this.cellType = cellType;
+            this.ssid = ssid;
+            this.bSSID = bSSID;
+            this.strength = strength;
+            this.capabilities = capabilities;
+            this.password = password;         
+        }
+
+        public string theIdentifier {
+            get { return this.identifier; } 
+        }
+        
+        public int theCellType { get => this.cellType; }
+
+        public string theSSID { get => this.ssid; }
+
+        public string theBSSID => this.bSSID;
+
+        public string thePassword { get => this.password; }
+
+        public string theStrength => this.strength;
+
+        public string theCapabilities => this.capabilities;
+
+        public override string ToString() {
+            return "SAccessPointImpl {Identifier: " + theIdentifier + ", SSID: " + theSSID + ", BSSID: " + theBSSID 
+            + ", Password: " + thePassword + ", Strength: " + theStrength + ", Capabilities: " + theCapabilities + "}";
+        }
+    }
+}

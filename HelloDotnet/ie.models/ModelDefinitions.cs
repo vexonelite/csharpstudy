5fdb3b126d8603819fd80ee9b9eef7ab2e95836d
elite_lin <elite_lin@goglobal.com.tw>
2020-12-07 11:44:40 +0800

    add namespace ie.models {}

---
+++ HelloDotnet/ie.models/ModelDefinitions.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;   // need for IList<T>, List<T>, IDictionary<K, V> Dictionary<K, V>
+using ie.delegates;
+using ie.delegates.reactives;
+
+
+namespace ie.models 
+{
+    
+}

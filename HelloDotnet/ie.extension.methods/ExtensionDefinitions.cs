fb53e3f17be398ffebdc8efb155a69549f77c659
elite_lin <elite_lin@goglobal.com.tw>
2021-02-09 11:21:06 +0800

    add     public static class DictExtensions {}:     add         public static void Append<Key, Value>(this IDictionary<Key, Value> destnation, IDictionary<Key, Value> source) {}

--- HelloDotnet/ie.extension.methods/ExtensionDefinitions.cs
+++ HelloDotnet/ie.extension.methods/ExtensionDefinitions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+
 
 namespace ie.extension.methods
 {    
@@ -19,6 +21,20 @@ namespace ie.extension.methods
         public static int WordCount(this string s) {
             return s.Split(new char[] {' ', '.','?'}, StringSplitOptions.RemoveEmptyEntries)
                     .Length;
+        }
+    }
+
+    public static class DictExtensions {
+        /** 
+         * [Reference](https://www.techiedelight.com/add-contents-of-dictionary-to-another-dictionary-csharp/)
+         */
+        public static void Append<Key, Value>(this IDictionary<Key, Value> destnation, IDictionary<Key, Value> source) {
+            foreach (KeyValuePair<Key, Value> item in source) {
+                if (destnation.ContainsKey(item.Key)) {  
+                    Console.WriteLine("DictExtensions - Append: Dictionary destnation has contained key: {0} --> override !!", item.Key);
+                }             
+                destnation[item.Key] = item.Value;
+            }
         }
     }
 }
